fde6da933b94cb5f34b42bc5151a8369e7207402
apundir <apundir@campus.uni-paderborn.de>
2021-05-14 14:25:25 +0200

    Max Vertex Metric Logic

--- src/main/java/org/aksw/simba/lemming/metrics/single/edgemanipulation/EdgeModifier.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/edgemanipulation/EdgeModifier.java
@@ -1,9 +1,12 @@
 package org.aksw.simba.lemming.metrics.single.edgemanipulation;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import org.aksw.simba.lemming.ColouredGraph;
+import org.aksw.simba.lemming.metrics.single.MaxVertexDegreeMetric;
 import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
 import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
 import org.aksw.simba.lemming.tools.PrecomputingValues;
@@ -11,7 +14,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.carrotsearch.hppc.ObjectDoubleOpenHashMap;
+import com.github.andrewoma.dexx.collection.Map;
 
+import grph.Grph.DIRECTION;
+import it.unimi.dsi.fastutil.ints.IntCollection;
+import it.unimi.dsi.fastutil.ints.IntIterator;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 
 public class EdgeModifier {
@@ -27,6 +35,8 @@ public class EdgeModifier {
 	private List<TripleBaseSingleID> mLstAddedEdges;
 	private boolean isCoutingEdgeTriangles = false;
 	private boolean isCountingNodeTriangles = false;
+	private HashMap<String, IntSet> mMapCandidatesMaxDegMetric; //Map for storing candidate vertices for Max Degree metric computation
+	private HashMap<String, Double> mMapCandidatesMaxDegreeMetricValues;
 	
 	public EdgeModifier(ColouredGraph clonedGraph, List<SingleValueMetric> lstMetrics){
 		//list of metric
@@ -38,6 +48,22 @@ public class EdgeModifier {
 		computeMetricValues(clonedGraph, lstMetrics);
 		//initialize EdgeModification
 		mEdgeModification= new EdgeModification(clonedGraph,(int) mMapMetricValues.get("#nodetriangles"),(int) mMapMetricValues.get("#edgetriangles"));
+		
+		//Initialize Hash map for candidate vertices (MaxVertexDegreeMetric).
+		mMapCandidatesMaxDegMetric = new HashMap<>();
+		mMapCandidatesMaxDegMetric.put("RemoveAnEdgeIndegree", new IntOpenHashSet());
+		mMapCandidatesMaxDegMetric.put("RemoveAnEdgeOutdegree", new IntOpenHashSet());
+		mMapCandidatesMaxDegMetric.put("AddAnEdgeIndegree", new IntOpenHashSet());
+		mMapCandidatesMaxDegMetric.put("AddAnEdgeOutdegree", new IntOpenHashSet());
+		
+		//Initialize Hash map for storing maximum vertex degrees for different cases.
+		mMapCandidatesMaxDegreeMetricValues = new HashMap<>();
+		mMapCandidatesMaxDegreeMetricValues.put("RemoveAnEdgeIndegree", 0.0);
+		mMapCandidatesMaxDegreeMetricValues.put("RemoveAnEdgeOutdegree", 0.0);
+		mMapCandidatesMaxDegreeMetricValues.put("AddAnEdgeIndegree", 0.0);
+		mMapCandidatesMaxDegreeMetricValues.put("AddAnEdgeOutdegree", 0.0);
+		
+		
 	}
 	
 	private void computeMetricValues(ColouredGraph clonedGraph, List<SingleValueMetric> lstMetrics){
@@ -82,7 +108,7 @@ public class EdgeModifier {
 				triple.tailId != -1 && triple.headId !=-1){
 			
 			//add to list of removed edges
-			//mLstRemovedEdges.add(triple);
+			mLstRemovedEdges.add(triple);
 			
 			ObjectDoubleOpenHashMap<String> mapChangedMetricValues = new ObjectDoubleOpenHashMap<String>();
 			
@@ -102,19 +128,21 @@ public class EdgeModifier {
 	        for(SingleValueMetric metric: mLstMetrics){
 	        	if(!metric.getName().equalsIgnoreCase("#edgetriangles") &&
 	        			!metric.getName().equalsIgnoreCase("#nodetriangles")){
-        			double metVal = metric.apply(graph);
+        			//double metVal = metric.apply(graph);
+        			double metVal = tryToRemoveAnEdgeMetricComputation(triple, metric, graph); // Calling the Metric computation method
 	        		mapChangedMetricValues.put(metric.getName(), metVal);   
 	        	}
 	        }
 	        
 	        //reverse the graph
 	       // mEdgeModification.addEdgeToGraph(triple.tailId, triple.headId, triple.edgeColour);
-	        int removedEdgeID = mEdgeModification.addEdgeToGraph(triple.tailId, triple.headId, triple.edgeColour, 
+	        mEdgeModification.addEdgeToGraph(triple.tailId, triple.headId, triple.edgeColour, 
 	        		(int)mMapMetricValues.get("#nodetriangles"),(int) mMapMetricValues.get("#edgetriangles"));
 	        
-	        TripleBaseSingleID removedTriple = new TripleBaseSingleID(triple.tailId, triple.headColour, triple.headId, 
-	        		triple.tailColour, removedEdgeID, triple.edgeColour);
-	        mLstRemovedEdges.add(removedTriple);
+	        IntSet curEdges = mEdgeModification.getGraph().getEdges();
+			if(!curEdges.contains(triple.edgeId)) {
+				return null;
+			}
 	        
 	        return mapChangedMetricValues;
 		}else{
@@ -146,7 +174,8 @@ public class EdgeModifier {
 	        	if(!metric.getName().equalsIgnoreCase("#edgetriangles") &&
 	        			!metric.getName().equalsIgnoreCase("#nodetriangles")){
 	        		
-        			double metVal = metric.apply(graph);
+        			//double metVal = metric.apply(graph);
+        			double metVal = tryToAddAnEdgeMetricComputation(triple, metric, graph);
 	        		mapMetricValues.put(metric.getName(), metVal);   
 	        	}
 	        }
@@ -210,5 +239,76 @@ public class EdgeModifier {
 	
 	public ObjectDoubleOpenHashMap<String> getOptimizedMetricValues(){
 		return mMapMetricValues;
+	}
+	
+	private double tryToRemoveAnEdgeMetricComputation(TripleBaseSingleID triple, SingleValueMetric metric, ColouredGraph graph) {
+		double metVal = 0;
+		
+		if(metric.getName().equals("maxInDegree")) { 
+			
+			metVal = metricComputationMaxDegree(metric, graph, "RemoveAnEdgeIndegree", DIRECTION.in, triple.headId);
+			// Logic to check if maxDegreeMetric call is required for maxInDegree, defined in method : metricComputationMaxDegree
+			
+		}else if (metric.getName().equals("maxOutDegree")) {
+			
+			metVal = metricComputationMaxDegree(metric, graph, "RemoveAnEdgeOutdegree", DIRECTION.out, triple.tailId);
+			
+		}else { // If metric is other than maxInDegree and maxOutDegree then apply the metric
+			metVal = metric.apply(graph);
+		}
+		
+		return metVal;
+	}
+	
+	private double tryToAddAnEdgeMetricComputation(TripleBaseSingleID triple, SingleValueMetric metric, ColouredGraph graph) {
+		double metVal = 0;
+		
+		if(metric.getName().equals("maxInDegree")) { // Logic to check if maxDegreeMetric call is required for maxInDegree
+			
+			metVal = metricComputationMaxDegree(metric, graph, "AddAnEdgeIndegree", DIRECTION.in, triple.headId);
+				
+			
+		}else if (metric.getName().equals("maxOutDegree")) {
+			
+			metVal = metricComputationMaxDegree(metric, graph, "AddAnEdgeOutdegree", DIRECTION.out, triple.tailId);
+			
+		}else { // If metric is other than maxInDegree and maxOutDegree then apply the metric
+			metVal = metric.apply(graph);
+		}
+		
+		return metVal;
+	}
+	
+	private double metricComputationMaxDegree(SingleValueMetric metric, ColouredGraph graph, String metricName, DIRECTION direction, int tripleID) {
+		double metVal;
+		
+		IntSet intSetTemp = mMapCandidatesMaxDegMetric.get(metricName);//Get the current candidate set
+		
+		if(intSetTemp.size() == 0) { //Initially the Candidate set will be empty, hence need to call the apply method and store the candidates
+			
+			metVal = metric.apply(graph); // apply the metric and get the value
+			mMapCandidatesMaxDegreeMetricValues.replace(metricName, metVal); // Store the metric value for later use
+			IntSet maxDegreeVertices = graph.getGraph().getVerticesOfDegree((int)metVal, direction); // Get the vertex with the metric value 
+			intSetTemp.addAll(maxDegreeVertices); // store the vertex with metric value in candidate set
+			
+		}else {
+			
+			if(intSetTemp.contains(tripleID)) { // The Edge for vertex in candidate set is modified
+				metVal = metric.apply(graph); // apply the metric and get the value
+				mMapCandidatesMaxDegreeMetricValues.replace(metricName, metVal); // Store the metric value for later use
+				IntSet maxDegreeVertices = graph.getGraph().getVerticesOfDegree((int)metVal, direction); // Get the vertex with the metric value 
+				intSetTemp.addAll(maxDegreeVertices); // store the vertex with metric value in candidate set
+			}
+			else { // If Edge for vertex in candidate set is not modified then we can use the previously stored values
+				metVal = mMapCandidatesMaxDegreeMetricValues.get(metricName); 
+				int inVertexDegreeTemp = graph.getGraph().getInVertexDegree(tripleID); // Check the vertex degree for new edge
+				if(inVertexDegreeTemp == metVal) { 
+					intSetTemp.add(inVertexDegreeTemp); // If vertex has a degree similar to metric value previously stored, add the vertex in candidate set
+				}
+				
+			}
+		}
+		
+		return metVal;
 	}
 }
